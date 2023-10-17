35b88b5869c3efed8d142498c577b6f6e8af46bb
Roadagain <ringoh72@gmail.com>
2023-10-18 01:08:25 +0900

    全体を統括して読み順に出力する

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import sys
 import csv
+from functools import reduce
 
 def read_ime_dict_item(row):
     return {
@@ -24,10 +25,11 @@ def main():
         print(f'Usage: python3 {sys.argv[0]} <filename1> ... <filenameN>')
         sys.exit(0)
 
-    for filename in sys.argv[1:]:
-        ime_dict = read_ime_dict_from_csv(filename)
-        for item in ime_dict:
-            print_ime_dict_item(item)
+    ime_dicts = [read_ime_dict_from_csv(filename) for filename in sys.argv[1:]]
+    unified_ime_dict = sorted(reduce(lambda x, y: x + y, ime_dicts), key=lambda x: x['kana'])
+
+    for item in unified_ime_dict:
+        print_ime_dict_item(item)
 
 if __name__ == '__main__':
     main()
