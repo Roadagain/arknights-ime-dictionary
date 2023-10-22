e6df8b68e1ed734986461fbdbca832e9fff55f5c
Roadagain <ringoh72@gmail.com>
2023-10-22 12:27:37 +0900

    改行コードがCRLFになっていたのでLFだけにする

--- create-ime-dict.py
+++ create-ime-dict.py
@@ -17,7 +17,7 @@ def main():
     ime_dicts = [read_ime_dict_from_csv(filename) for filename in sys.argv[1:]]
     unified_ime_dict = sorted(reduce(lambda x, y: x + y, ime_dicts), key=lambda x: x['読み'])
 
-    writer = csv.DictWriter(sys.stdout, fieldnames=['読み', '単語', '品詞', 'コメント'], extrasaction='ignore', delimiter='\t')
+    writer = csv.DictWriter(sys.stdout, fieldnames=['読み', '単語', '品詞', 'コメント'], extrasaction='ignore', delimiter='\t', lineterminator='\n')
     writer.writerows(unified_ime_dict)
 
 if __name__ == '__main__':
